a9620dd1e2b1033796312eb8fa8c40a590eea821
Erick <adolfo.solis.is@unipolidgo.edu.mx>
2023-05-30 10:38:54 -0600

    start with years table

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,7 +1,7 @@
-import { Component } from 'solid-js';
-import { createColumnHelper, createSolidTable } from '@tanstack/solid-table';
+import { Component, For, createSignal } from 'solid-js';
+import { ColumnDef, createColumnHelper, createSolidTable, flexRender, getCoreRowModel } from '@tanstack/solid-table';
 
-type Person = {
+type Year = {
     year_id: string
     from_date: string
     to_date: string
@@ -10,11 +10,118 @@ type Person = {
     months: number
   }
   
-  const columnHelper = createColumnHelper<Person>()
+  const defaultData: Year[] = [
+    {
+      year_id: '1',
+      from_date: '2018',
+      to_date: '2019',
+      employees: 7,
+      complete: true,
+      months: 12
+    }
+  ]
 
+  const columnHelper = createColumnHelper<Year>()
+  const defaultColumns = [
+    columnHelper.accessor( 'year_id', {
+      cell: info => info.getValue(),
+    }),
+    columnHelper.accessor( 'from_date', {
+      cell: info => info.getValue(),
+      header: () => <span>Incio</span>,
+    }),
+    columnHelper.accessor( 'to_date', {
+      cell: info => info.getValue(),
+      header: () => <span>Termino</span>,
+    }),
+    columnHelper.accessor( 'employees', {
+      cell: info => info.getValue(),
+      header: () => <span>Empleados</span>,
+    }),
+    columnHelper.accessor( 'complete', {
+      cell: info => info.getValue() ? 'Completado' : 'En Progreso',
+      header: () => <span>Progreso</span>,
+    }),
+
+  ]
 const DataTable: Component<{}> = (props) => {
-  //const table = createSolidTable(options);
-  return <div></div>;
+  const [data, setData] = createSignal(defaultData)
+  const rerender = () => setData(defaultData)
+
+  const table = createSolidTable({
+    get data() {
+      return data()
+    },
+    columns: defaultColumns,
+    getCoreRowModel: getCoreRowModel(),
+  })
+  return  (<div class="p-2">
+  <table>
+    <thead>
+      <For each={table.getHeaderGroups()}>
+        {headerGroup => (
+          <tr>
+            <For each={headerGroup.headers}>
+              {header => (
+                <th>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </th>
+              )}
+            </For>
+          </tr>
+        )}
+      </For>
+    </thead>
+    <tbody>
+      <For each={table.getRowModel().rows}>
+        {row => (
+          <tr>
+            <For each={row.getVisibleCells()}>
+              {cell => (
+                <td>
+                  {flexRender(
+                    cell.column.columnDef.cell,
+                    cell.getContext()
+                  )}
+                </td>
+              )}
+            </For>
+          </tr>
+        )}
+      </For>
+    </tbody>
+    <tfoot>
+      <For each={table.getFooterGroups()}>
+        {footerGroup => (
+          <tr>
+            <For each={footerGroup.headers}>
+              {header => (
+                <th>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.footer,
+                        header.getContext()
+                      )}
+                </th>
+              )}
+            </For>
+          </tr>
+        )}
+      </For>
+    </tfoot>
+  </table>
+  <div class="h-4" />
+  <button onClick={() => rerender()} class="border p-2">
+    Rerender
+  </button>
+</div>
+);
 };
 
 export default DataTable;
--- src/pages/Finances.tsx
+++ src/pages/Finances.tsx
@@ -1,10 +1,12 @@
 import { Component } from "solid-js";
+import DataTable from "../components/DataTable";
 
 const Finances: Component<{}> = (props) => {
   
   return(
   <div class="m-2 border w-full rounded border-slate-400 border-dashed p-4">
     <h1 class="lg:text-5xl text-4xl font-bold mb-1">Años</h1>
+    <DataTable />
     <hr />
   </div>);
 };
